0a8c4652052e7a2e9362d21c6ce0626a24b7e832
Adithya R <rsadithya12@gmail.com>
2023-02-16 22:33:39 +0530

    get methods added

--- DataToAction/DtoaBackend/routes/user.js
+++ DataToAction/DtoaBackend/routes/user.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const router  = express.Router()
+const mongo = require('../database')
 
-router.post('/create-doc',(req,res,next) => {
-    console.log(req)
+router.get('/all-template', async (req,res,next) => {
+    try{
+        var data = await mongo.selectedDb.collection('templates').find().toArray()
+        res.status(200).send(data)
+    }catch(err){
+        res.status(500).send(err)
+    }
 })
 
-router.get('/receive-doc',(req,res,next) => {
-    console.log(req)
+router.get('/template/:title',async (req,res,next) => {
+    try{
+        var data = await mongo.selectedDb.collection('templates').find({title:req.params.title}).toArray()
+        res.status(200).send(data[0])
+    }catch(err){
+        res.status(500).send(err)
+    }
 })
 
 module.exports = router;
