7a176c87f12bd38d23c13d911b920ea49591ae68
Adithya R <rsadithya12@gmail.com>
2023-02-16 20:23:39 +0530

    Admin post method added

--- DataToAction/DtoaBackend/routes/admin.js
+++ DataToAction/DtoaBackend/routes/admin.js
@@ -1,19 +1,16 @@
 const express = require('express')
 const router  = express.Router()
+const mongo = require('../database')
 
-router.post('/add-template',(req,res,next) => {
-    console.log(req.url);
-    console.log("LOGGED");
-
-    //Do some things
-
-
-    const vararray = [];
-    res.send(varArray);
+router.post('/add-template', async (req,res,next) => {
+    try{
+        var data = await mongo.selectedDb.collection('templates').insertOne(req.body)
+        res.status(200).send(data)
+    }catch(err) {
+        console.log(err);
+        res.status(500).send(err);
+    }
 })
 
-router.post('/add-dataType', (req,res,next) => {
-    console.log(req);
-})
 
 module.exports = router;
