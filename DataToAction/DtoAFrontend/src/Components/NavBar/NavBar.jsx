bd370085dbbe43f36a1dd0096ca2aa07281fa5c9
Dananjay996 <sreedananjay456@gmail.com>
2023-02-16 12:04:20 +0530

    NavBar fix

--- DataToAction/DtoAFrontend/src/Components/NavBar/NavBar.jsx
+++ DataToAction/DtoAFrontend/src/Components/NavBar/NavBar.jsx
@@ -1,10 +1,20 @@
+import {useState} from 'react';
 import React from 'react'
 import './NavBar.css';
+import { HiMenuAlt4 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+
+const NavBarItem = ({title,className}) => {
+  <li className = {className}> {title} </li>
+}
 
 const NavBar = () => {
+
+  const [toggleMenu,setTogglMenu] = useState(false);
+
   return (
     <div>
-      NavBar
+      
     </div>
   )
 }
