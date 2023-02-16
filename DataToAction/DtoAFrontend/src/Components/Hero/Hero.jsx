b7023e36e8239a79a19a8e5d7197bf153a4eec9f
Adithya R <rsadithya12@gmail.com>
2023-02-16 12:52:17 +0530

    Added Hero

--- DataToAction/DtoAFrontend/src/Components/Hero/Hero.jsx
+++ DataToAction/DtoAFrontend/src/Components/Hero/Hero.jsx
@@ -3,9 +3,33 @@ import './Hero.css';
 
 const Hero = () => {
   return (
-    <div>
-        Hero
-      
+    <div className='DTA__hero_container'>
+      <div className='DTA__hero_row'>
+        <div className='DTA__hero_image_container'>
+          <img src='https://dummyimage.com/500x500/000/fff'/>
+        </div>
+        <div className="DTA__hero_content_container">
+          <div className='DTA__hero_content_title'>
+            Creating a Template
+          </div>
+          <div className='DTA__hero_content_desc'>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet, rem temporibus esse laboriosam tenetur possimus ipsam ducimus eos sequi assumenda maxime, voluptatum officiis quasi nihil voluptate asperiores iure aperiam. Vita. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi nisi error libero? Quod magnam hic inventore necessitatibus, suscipit voluptas est velit earum ipsam vitae, aspernatur quia nisi molestiae iure delectus?
+          </div>
+        </div>
+      </div>
+      <div className='DTA__hero_row'>
+        <div className="DTA__hero_content_container">
+        <div className='DTA__hero_content_title'>
+            Using a Template
+          </div>
+          <div className='DTA__hero_content_desc'>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet, rem temporibus esse laboriosam tenetur possimus ipsam ducimus eos sequi assumenda maxime, voluptatum officiis quasi nihil voluptate asperiores iure aperiam. Vita. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi nisi error libero? Quod magnam hic inventore necessitatibus, suscipit voluptas est velit earum ipsam vitae, aspernatur quia nisi molestiae iure delectus?
+          </div>
+        </div>
+        <div className="DTA__hero_image_container">
+          <img src='https://dummyimage.com/500x500/000/fff'/>
+        </div>
+      </div>
     </div>
   )
 }
