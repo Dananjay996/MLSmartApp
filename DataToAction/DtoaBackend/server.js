e13c8c9663a879c74d63a5484848c8554a91f014
Adithya R <rsadithya12@gmail.com>
2023-02-16 20:23:16 +0530

    database connected

--- DataToAction/DtoaBackend/server.js
+++ DataToAction/DtoaBackend/server.js
@@ -1,8 +1,15 @@
 const http = require('http');
-
+const dotenv = require("dotenv")
+const mongo = require("./database")
 const express = require('express');
+const cors = require('cors')
+
+dotenv.config()
 
 const app = express();
+app.use(express.json())
+app.use(cors())
+mongo.connect()
 const adminRoutes = require('./routes/admin')
 const userRoutes = require('./routes/user')
 
@@ -12,4 +19,4 @@ app.use(userRoutes);
 app.use('/',(req,res,next) => {
     res.status(404).send('<h1>Page not found</h1>')
 })
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
