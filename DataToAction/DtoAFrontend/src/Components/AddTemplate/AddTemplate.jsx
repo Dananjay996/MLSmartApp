e614ed345551b6f2bbacfe7ae376e6f8bec8de48
Dananjay996 <sreedananjay456@gmail.com>
2023-02-16 16:28:43 +0530

    Changed some routes in frontend and backend

--- DataToAction/DtoAFrontend/src/Components/AddTemplate/AddTemplate.jsx
+++ DataToAction/DtoAFrontend/src/Components/AddTemplate/AddTemplate.jsx
@@ -1,7 +1,41 @@
 import React from 'react'
 import './AddTemplate.css'
-export default function AddTemplate() {
+import axios from 'axios';
+
+const AddTemplate = () => {
+
+  const [selectedFile,setSelectedFile] = React.useState(null);
+
+  const handleFileSelect = (e) => {
+    setSelectedFile(e.target.files[0]);
+  }
+
+  const submitHandler = async (e) => {
+    // console.log(e.target.files[0]);
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("selectedFile", selectedFile);
+
+    try{
+      const response = await axios({
+        method : "post",
+        url : "http://localhost:3000/add-template",
+        data : formData,
+        headers : {"Content-Type" : "multipart/form-data"},
+      });
+    }
+    catch(err){
+      console.log(err);
+    }
+  }
+
   return (
-    <div>AddTemplate</div>
+    <form onSubmit={submitHandler}>
+      <input type="file" name="WordFile" onChange = {handleFileSelect}/>
+      <button type="submit">Submit</button>
+    </form>
   )
 }
+
+export default AddTemplate;
--- DataToAction/DtoaBackend/routes/admin.js
+++ DataToAction/DtoaBackend/routes/admin.js
@@ -2,7 +2,18 @@ const express = require('express')
 const router  = express.Router()
 
 router.post('/add-template',(req,res,next) => {
-    console.log(req)
+    console.log(req.url);
+    console.log("LOGGED");
+
+    //Do some things
+
+
+    const vararray = [];
+    res.send(varArray);
+})
+
+router.post('/add-dataType', (req,res,next) => {
+    console.log(req);
 })
 
 module.exports = router;
