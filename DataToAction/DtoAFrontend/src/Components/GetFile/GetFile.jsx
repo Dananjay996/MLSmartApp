39465cadeb2968d4bb11742f26263f651d50fa3f
Dananjay996 <sreedananjay456@gmail.com>
2023-02-16 23:28:31 +0530

    getFile

--- DataToAction/DtoAFrontend/src/Components/GetFile/GetFile.jsx
+++ DataToAction/DtoAFrontend/src/Components/GetFile/GetFile.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
 import UseTemplate from '../Hero/UseTemplate';
 import axios from 'axios';
 
 const GetFile = () => {
+
+  const [data,setData] = useState([]);
+  const [filteredTitles,setFilteredTitles] = useState([]);
+  const [selectedTitle,setSelectedTitle] = useState("");
+
   return (
     <div>
       <UseTemplate />
+
     </div>
   )
 }
