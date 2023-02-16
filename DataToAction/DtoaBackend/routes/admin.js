464f32b9b5d2c977620f0c96d58e22e1e93e24dd
Adithya R <rsadithya12@gmail.com>
2023-02-16 22:33:19 +0530

    Template already exist section added

--- DataToAction/DtoaBackend/routes/admin.js
+++ DataToAction/DtoaBackend/routes/admin.js
@@ -4,6 +4,11 @@ const mongo = require('../database')
 
 router.post('/add-template', async (req,res,next) => {
     try{
+        var {title,content,inputfields} = req.body
+        const oldTemplate = await mongo.selectedDb.collection("templates").find({title:title}).toArray()
+        if(oldTemplate.length>0){
+            return res.status(409).send("Template already exist!")
+        }
         var data = await mongo.selectedDb.collection('templates').insertOne(req.body)
         res.status(200).send(data)
     }catch(err) {
